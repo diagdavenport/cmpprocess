d756c1df3caae6951b2215fef36391308a62d88a
diagdavenport <diag.davenport@gmail.com>
2017-02-28 21:25:09 -0500

    Documentation update

--- R/CMPProcess.r
+++ R/CMPProcess.r
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #' COM Poisson Process Parameter Estimation.
 #'
 #' \code{CMPProcess} computes the maximum likelihood estimates of a COM-Poisson process for given count data.
@@ -12,42 +11,6 @@
 #'
 #' @examples
 #' ## Standard usage
-=======
-#' COM Poisson Process Parameter Estimation
-#' 
-#' \code{CMPProcess} computes the maximum likelihood estimates of a COM-Poisson process for given count data
-#' 
-#' @param counts A vector (like object) of counts
-#' @param s The time interval corresponding to the levels of the counts vector. Note that s must be a positive integer.
-#' 
-#' @return If the time interval (s) is 1, then CMPProcess will return: se=se, H=H, lambda=lambda, nu=nu, Z=Z, ll=loglike, aic=(4-2*loglike).
-#'     
-#'     lambda \deqn{\ln L(\lambda, \nu | x_1,\cdots,x_n)= (\ln \lambda)\sum\limits _{i=1}^n x_i - \nu\sum\limits _{i=1}^n \ln(x_i!) - n\ln Z(\lambda,\nu)}
-#'     nu
-#'     Z
-#'    
-#'     se
-#'     H 
-#'     
-#'     ll
-#'     aic
-#'         
-#'           
-#'    Otherwise, if the time interval (s) is greater than 1, CMPProcess will return lambda=lambda, nu=nu, ll=loglike, aic=(4-2*loglike)
-#'     
-#'     Not sure I'm clear on the form of the ll function of sCMP process. What exactly is the dSCMP function doing?
-#'     
-#'     lambda
-#'     nu
-#'     ll
-#'     aic
-#'     
-#'     
-#' 
-#' @examples 
-#' 
-#' ## Standard use
->>>>>>> origin/master
 #' data(floodcount)
 #' CMPProcess(floodcount$Counts)
 #'
@@ -87,18 +50,18 @@ CMPProc <- function(counts, h.out= F)
     total <- sum(log(factorial(counts)))
     -(log(par[1]) * sum(counts) - par[2] * total - length(counts) * com.compute.log.z(par[1],par[2],exp(-20)))
   }
-
+  
   LambdaNuEst <- nlminb(start = c(1,1), LambdaNuPair, lower = c(exp(-10),exp(-10)), upper =c(Inf, Inf))
-
+  
   if (h.out == TRUE) H <- hessian(LambdaNuPair, LambdaNuEst$par)
   else               H <- NA
-
+  
   se <- sqrt(diag(solve(H)))
   lambda <- LambdaNuEst$par[1]
   nu <- LambdaNuEst$par[2]
   Z <- com.compute.z(lambda,nu)
   loglike <- -LambdaNuEst$objective
-
+  
   cat("log likelihood:",loglike,"\n")
   cat("lambda:",lambda,"\n")
   cat("nu:",nu,"\n")
@@ -107,7 +70,7 @@ CMPProc <- function(counts, h.out= F)
   cat("real dispersion:", var(counts)/mean(counts),"\n")
   cat("waiting time distribution is geometric with parameter:", 1-1/Z,"\n")
   cat("AIC:",4-2*loglike,"\n")
-
+  
   return(list(se=se, H=H, lambda=lambda, nu=nu, Z=Z, ll=loglike, aic=(4-2*loglike)))
 }
 
@@ -125,7 +88,7 @@ SCMPProc <- function(counts, s, h.out = F)
 {
   LambdaNuPair <- function(par)
   {
-
+    
     w <- 1
     total <- 0
     while(w <= length(counts))
@@ -133,21 +96,21 @@ SCMPProc <- function(counts, s, h.out = F)
       total <- total +log(dSCMP(s,par[1],par[2],counts[w]))
       w <- w + 1
     }
-
+    
     -total
   }
-
+  
   LambdaNuEst <- nlminb(start = c(.01,.01), LambdaNuPair,lower = c(exp(-20),exp(-20)), upper =c(Inf, Inf),control=list(trace=1))
-
+  
   if (h.out == TRUE)   H <- hessian(LambdaNuPair, LambdaNuEst$par)
   else                 H <- NA
-
+  
   se <- sqrt(diag(solve(H)))
   lambda <- LambdaNuEst$par[1]
   nu <- LambdaNuEst$par[2]
   loglike <- -LambdaNuEst$objective
   Z <- com.compute.z(lambda,nu)
-
+  
   cat("log likelihood:",loglike,"\n")
   cat("lambda:",lambda,"\n")
   cat("nu:",nu,"\n")
@@ -155,7 +118,7 @@ SCMPProc <- function(counts, s, h.out = F)
   cat("nu se:",se[2],"\n")
   cat("waiting time distribution is geometric with parameter:", 1-1/(com.compute.z(lambda,nu)),"\n")
   cat("AIC:",4-2*loglike,"\n")
-
+  
   return(list(se=se, H=H, lambda=lambda, nu=nu, Z=Z, ll=loglike, aic=(4-2*loglike)))
 }
 
@@ -172,16 +135,16 @@ SCMPProc <- function(counts, s, h.out = F)
 
 dSCMP <- function(NuOfVar,Lambda,Nu,count)
 {
-
+  
   lambda <- rep.int(Lambda,NuOfVar)
   nu <- rep.int(Nu,NuOfVar)
-
+  
   if(length(lambda) == 1)
   {
     print("NuOfVar must be > 1. If interested in computing probability where NuOfVar = 1, run dcom in compoisson package.")
     return("TRY AGAIN!")
   }
-
+  
   m <- 1
   while(m <= length(lambda))
   {
@@ -192,11 +155,11 @@ dSCMP <- function(NuOfVar,Lambda,Nu,count)
     }
     m <- m + 1
   }
-
+  
   a <- numeric(0)
   keep <- numeric(0)
   total <- 0
-
+  
   if (length(lambda) == 2)
   {
     a[1] <- 0
@@ -210,7 +173,7 @@ dSCMP <- function(NuOfVar,Lambda,Nu,count)
     result <- 1/(com.compute.z(lambda[1],nu[1],exp(-30))*(com.compute.z(lambda[2],nu[2],exp(-30))))* sum(keep)
     return(result)
   }
-
+  
   recur <- function(number,iterator)
   {
     a <- numeric(0)
@@ -220,7 +183,7 @@ dSCMP <- function(NuOfVar,Lambda,Nu,count)
     storage[[1]] <- numeric(0)
     sum1 <- 0
     sum2 <- 0
-
+    
     if (number == 2)
     {
       a[1] <- 0
@@ -250,7 +213,7 @@ dSCMP <- function(NuOfVar,Lambda,Nu,count)
         return(sum(storage[[t]]))
       }
     }
-
+    
     w <- 1
     result2 <- 1
     while(w <= length(lambda))
@@ -264,8 +227,8 @@ dSCMP <- function(NuOfVar,Lambda,Nu,count)
     #cat("Prob of",count,"counts: ", result2,"\n")
     return(result2)
   }
-
-
-
+  
+  
+  
   recur(length(lambda),count)
 }
